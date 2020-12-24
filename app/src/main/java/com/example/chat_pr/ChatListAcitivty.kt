8cf3681c665e088536fc8854394fcfcbb0ada9e1
ckrudals <2004ckck@naver.com>
2020-12-24 22:20:38 +0900

    12-24

---
+++ app/src/main/java/com/example/chat_pr/ChatListAcitivty.kt
@@ -0,0 +1,12 @@
+package com.example.chat_pr
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ChatListAcitivty : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chatlist)
+
+    }
+}
